6aee4944e8fad22c83ea67ac8d9ae9927e897b4c
Joshua <joshua-koeck@gmx.de>
2022-03-29 21:16:54 +0200

    b

--- sdk/thirdparty/github.py
+++ sdk/thirdparty/github.py
@@ -5,7 +5,6 @@ def get_current_github_user(access_token: str) -> dict:
     headers = {"Authorization": f"Token {access_token}"}
 
     res = requests.request("GET", "https://api.github.com/user", headers=headers).json()
-
     return res
 
 
@@ -14,7 +13,7 @@ def get_repositories(access_token: str) -> dict:
     headers = {"Authorization": f"Token {access_token}"}
 
     res = requests.request(
-        "GET", f"https://api.github.com/{login}/repos", headers=headers
+        "GET", f"https://api.github.com/users/{login}/repos", headers=headers
     ).json()
 
     return res

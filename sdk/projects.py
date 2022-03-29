5e75973accd07c139b4814b8029df20c66f95bec
Joshua <joshua-koeck@gmx.de>
2022-03-29 19:36:14 +0200

    b

--- sdk/projects.py
+++ sdk/projects.py
@@ -1,48 +1,57 @@
 import time
 import uuid
-from .utils.data import retrieve_documents, delete_documents, insert_documents, update_documents
+from .utils.data import (
+    retrieve_documents,
+    delete_documents,
+    insert_documents,
+    update_documents,
+)
 from .utils.exceptions import ProjectNameAlreadyExistsException
 
 
-def create_project(name: str, account_id: str, tags: list = None, description: str = None):
+def create_project(
+    name: str, account_id: str, tags: list = None, description: str = None
+):
     """
     Create a new project for an organization/user
     """
-    same_project_name = bool(retrieve_documents(
-        "projects", "projects", {"name": name}))
+    same_project_name = bool(retrieve_documents("projects", "projects", {"name": name}))
     if not same_project_name:
-        insert_documents("projects", "projects", [
-            {
-                "type": "project",
-                "id": str(uuid.uuid4()),
-                "accountId": account_id,
-                # "owner": {
-                #    "type": account_type,
-                #    "login": account_login,
-                #    "avatarUrl": account_avatar_url,
-                #    # "_comment": "Call the users endpoint for more information"
-                # },
-                "name": name,
-                "tags": tags,
-                "description": description,
-                "updatedAt": int(time.time()),
-                "createdAt": int(time.time()),
-
-
-
-                # settings
-                "runnerClusters": [],
-                "environment": {},
-                "deploymentTriggerWebhooks": [],
-                "previewImage": None,
-                "publicUrl": None,
-
-                "domains":[],
-                "deploymentHooks":{},
-
-                "logActivityFor": ["newDeployment", "deleteDeployment", "updateSettings"],
-            }
-        ])
+        insert_documents(
+            "projects",
+            "projects",
+            [
+                {
+                    "type": "project",
+                    "id": str(uuid.uuid4()),
+                    "accountId": account_id,
+                    "name": name,
+                    "tags": tags,
+                    "description": description,
+                    "updatedAt": int(time.time()),
+                    "createdAt": int(time.time()),
+                    # source code from where exactly?!
+                    "gitProvider": None,
+                    "repository": None,
+                    "branch": "main",
+                    "deployOn": ["push", "pull"],
+                    "config": {"source": ""},  # or local config
+                    # settings
+                    "runnerClusters": [],
+                    "environment": {},
+                    "deploymentTriggerWebhooks": [],
+                    "previewImage": None,
+                    "publicUrl": None,
+                    "domains": [],
+                    "deploymentHooks": {},
+                    "logActivityFor": [
+                        "newDeployment",
+                        "deleteDeployment",
+                        "updateSettings",
+                    ],
+                }
+            ],
+        )
 
     else:
         raise ProjectNameAlreadyExistsException
@@ -50,23 +59,20 @@ def create_project(name: str, account_id: str, tags: list = None, description: s
 
 def get_projects(account_id: str, return_secret_data: bool = False):
     res = []
-    projects = retrieve_documents("projects", "projects", {
-                                  "accountId": account_id})
+    projects = retrieve_documents("projects", "projects", {"accountId": account_id})
 
     for project in projects:
         del project["_id"]
 
         if not return_secret_data:
-            project["environment"] = [
-                k for k, _v in project["environment"].items()]
+            project["environment"] = [k for k, _v in project["environment"].items()]
         res.append(project)
 
     return res
 
 
 def get_project(id: str, return_secret_data: bool = False):
-    res = retrieve_documents(
-        "projects", "projects", {"id": id})[0]
+    res = retrieve_documents("projects", "projects", {"id": id})[0]
     del res["_id"]
 
     if not return_secret_data:
@@ -79,5 +85,4 @@ def update_project(id: str, **kwargs) -> None:
     project = get_project(id)
     new_data = {**project, **kwargs}
 
-    update_documents("projects", "projects", {
-                     "id": id}, {"$set": new_data})
+    update_documents("projects", "projects", {"id": id}, {"$set": new_data})
--- sdk/thirdparty/github.py
+++ sdk/thirdparty/github.py
@@ -1,9 +1,20 @@
 import requests
 
 
-def get_current_github_user(access_token):
+def get_current_github_user(access_token: str) -> dict:
     headers = {"Authorization": f"Token {access_token}"}
 
     res = requests.request("GET", "https://api.github.com/user", headers=headers).json()
+
+    return res
+
+
+def get_repositories(access_token: str) -> dict:
+    login = get_current_github_user(access_token)["login"]
+    headers = {"Authorization": f"Token {access_token}"}
+
+    res = requests.request(
+        "GET", f"https://api.github.com/{login}/repos", headers=headers
+    ).json()
 
     return res

11d9e366afdc1396aef31050638a3e2a3800b831
Joshua <joshua-koeck@gmx.de>
2022-04-16 16:15:38 +0200

    github

--- sdk/thirdparty/github.py
+++ sdk/thirdparty/github.py
@@ -2,21 +2,21 @@ import requests
 import json
 
 
-def get_current_github_user(access_token: str) -> dict:
-    headers = {"Authorization": f"Token {access_token}"}
+class Github:
+    def __init__(self, token: str) -> None:
+        self.token = token
+        self.headers = {"Authorization": f"Token {token}"}
 
-    res = requests.request("GET", "https://api.github.com/user", headers=headers).json()
-    return res
+    def get_current_github_user(self) -> dict:
+        return requests.request(
+            "GET", "https://api.github.com/user", headers=self.headers
+        ).json()
 
-
-def get_repositories(access_token: str, query: dict = {}) -> dict:
-    login = get_current_github_user(access_token)["login"]
-    headers = {"Authorization": f"Token {access_token}"}
-    res = requests.request(
-        "GET",
-        f"https://api.github.com/users/{login}/repos",
-        params=json.dumps(query),
-        headers=headers,
-    ).json()
-
-    return res
+    def get_repositories(self, query: dict = {}) -> dict:
+        login = self.get_current_github_user()["login"]
+        return requests.request(
+            "GET",
+            f"https://api.github.com/users/{login}/repos",
+            params=json.dumps(query),
+            headers=self.headers,
+        ).json()

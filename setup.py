057dfa68802d65166ee7960b4a8678217ff26343
Joshua3212 <49119664+Joshua3212@users.noreply.github.com>
2022-05-27 15:53:26 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     author=sdk.__author__,
     author_email="some@mail.com",
     packages=["sdk", "sdk.thirdparty", "sdk.utils"],
-    python_requires=">=3.7",
+   # python_requires=">=3.7",
     install_requires=install_requires,
     project_urls={
         "GitHub": "https://github.com/hudduapp/sdk",

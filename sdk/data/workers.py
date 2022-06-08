2e764e788bac1a42e9463d20861da5f4cee74536
Joshua <joshua-koeck@gmx.de>
2022-06-08 18:23:52 +0200

    update workers

--- sdk/data/workers.py
+++ sdk/data/workers.py
@@ -10,13 +10,15 @@ class Workers(Template):
     def __init__(self, token: str) -> None:
         super().__init__(token, "workers", "workers")
 
-    def create(self, node_id: str, account_id: str, config: dict, status: str = "IDLE") -> dict:
+    def create(self, node_id: str, account_id: str, route: str, config: dict, status: str = "IDLE") -> dict:
         """
 
         :param node_id:
         :type node_id:
         :param account_id:
         :type account_id:
+        :param route:
+        :type route:
         :param config:
         :type config:
         :param status:
@@ -30,6 +32,7 @@ class Workers(Template):
                 "type": "worker",
                 "id": str(uuid.uuid4()),
                 "accountId": account_id,
+                "route": route,
                 "nodeId": node_id,
                 "status": status.upper(),
                 "config": config,

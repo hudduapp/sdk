c060a5ddf21eb4e7565fb05af24dadc7dd4325a4
Joshua <joshua-koeck@gmx.de>
2022-06-12 14:31:58 +0200

    better clusters support

--- sdk/data/clusters.py
+++ sdk/data/clusters.py
@@ -9,7 +9,7 @@ class Clusters(Template):
     def __init__(self, token: str, collection: str = "workers") -> None:
         super().__init__(token, "clusters", collection)
 
-    def register(self, node_id: str, meta: dict) -> None:
+    def register(self, node_id: str, merge: dict, config: dict) -> None:
         """
         A new item is only added if it doesn't exist already.
 
@@ -21,36 +21,40 @@ class Clusters(Template):
         >>>     _d = Data("data_token")
         >>>
         >>>     Clusters(_d.db.token, "cluster_name").register(
-        >>>         "node_id", {"meta_data": "as a dict"}
+        >>>         "node_id", {"merge_data": "merge data gets merged with the cluster dict an 0 depth" } {"config_data": "as a dict"}
         >>>     )
 
 
         :param node_id:
         :type node_id:
-        :param meta:
-        :type meta:
+        :param config:
+        :type config:
+        :param merge:
+        :type merge:
         :return:
         :rtype:
         """
+        cluster = {
+            "type": self.collection,
+            "nodeId": node_id,
+            "config": config,
+            "updatedAt": int(time.time()),
+            **merge,
+        }
         same_item_registered = self.db.retrieve({"nodeId": node_id})
 
         if not same_item_registered:
-            cluster = {
-                "type": self.collection,
-                "nodeId": node_id,
-                "meta": meta
-            }
+            cluster["id"] = str(uuid.uuid4())
+            cluster["createdAt"] = int(time.time)
+            cluster["token"] = str(secrets.token_hex(16))
+
             self.db.insert([cluster])
         else:
-            # Update meta data
-            self.update(
-                {
-                    "nodeId": node_id
-                },
-                {
-                    "meta": meta
-                }
-            )
+            # Update config data
+            self.update({"nodeId": node_id}, cluster)
+
+    def new_token(self, query: dict) -> dict:
+        self.update({"nodeId": node_id}, {"token": str(secrets.token_hex(16))})
 
     def teardown(self, query: dict) -> None:
         """
